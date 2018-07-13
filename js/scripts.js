9ac40087c9d5dff83885b016c20ff74f4dfc6935
tonnymuchui <tonykanyingah@gmail.com>
2018-07-13 13:01:08 +0300

    add correct

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,12 @@ $(document).ready(function(){
     event.preventDefault();
     var userinput = $("input#user").val();
     for (var i=1; i<=userinput; i++) {
-      var results = "";
+      var results =  "";
+      if (i % 3 != 0 && i % 5 !=0) results =(i)
       if (i % 3 === 0) results = "ping";
       if (i % 5 === 0) results += "pong";
       console.log(results || i);
-      $("p#display").append(results || i + '<br/>');
+      $("p#display").append(results + "<br/>" || i + '<br/>');
     }
   });
 });
